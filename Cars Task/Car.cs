e28e3d873b1fb0f389e83cb73a4ff7cbeb227960
Mahir Musayev <146106439+musayevmahir68@users.noreply.github.com>
2024-04-02 00:59:21 +0400

    encapculiton

---
+++ Cars Task/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_Task
+{
+    internal class Car
+    {
+        public int WheelCount { get; set; }
+        public int HorsePower { get; set; }
+    }
+}
